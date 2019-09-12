95a816f99d2d0ccdc3f354d19ba0215ad70fb243
“tingxinzhe” <tingxinzhe1@gmail.com>
2019-09-12 12:21:14 +0800

    done

--- b.py
+++ b.py
@@ -3,15 +3,16 @@
 import numpy as np
 import cv2
 
-img=np.zeros((500,1000),np.uint8)
+img = np.zeros((500, 1000), np.uint8)
 
 img.fill(240)
 
 for i in range(256):
-    img[i+100][120:180]=255-i
-for i in range(0,256,16):
-    img[i:i+16][220:280]=i
+    img[i + 100][120:180] = 255 - i
 
-cv2.imshow('image',img)
+for i in range(0, 256, 32):
+    img[i+100:i+132, 720:780] = 255-i
+
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
