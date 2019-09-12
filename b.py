0b83c3ee79897946bb51c9a01de9d2d9af62d0ba
“tingxinzhe” <tingxinzhe1@gmail.com>
2019-09-12 12:34:36 +0800

    done

--- b.py
+++ b.py
@@ -8,10 +8,10 @@ img = np.zeros((500, 1000), np.uint8)
 img.fill(245)
 
 for i in range(256):
-    img[i + 100][120:180] = 255 - i
+    img[i + 100][220:280] = 255 - i
 
 for i in range(0, 256, 32):
-    img[i+100:i+132, 720:780] = 255-i
+    img[i+100:i+132, 620:680] = 255-i
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
