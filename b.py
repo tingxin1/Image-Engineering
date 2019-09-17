bc1dd78e0af3788ca8c9e399d5825a7f64aa7b83
tingxin <tingxinzhe1@gmail.com>
2019-09-17 18:09:53 +0800

    1

--- b.py
+++ b.py
@@ -4,15 +4,17 @@
 import numpy as np
 import cv2
 
-img = np.zeros((500, 1000), np.uint8)
+img = np.zeros((400, 700,3), np.uint8)
 
 img.fill(245)
 
+# 生成渐变灰度图
 for i in range(256):
-    img[i + 100][220:280] = 255 - i
-
+    img[i + 50,120:180,:] = 255 - i
+    img[i+50,320:]
+# 生成等阶灰度图
 for i in range(0, 256, 32):
-    img[i+100:i+132, 620:680] = 255-i
+    img[i+50:i+82, 220:280,:] = 255-i
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
