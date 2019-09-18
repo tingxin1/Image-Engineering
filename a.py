af735f36dafbbc3fbbc0249e31296acc44adcf80
“tingxinzhe” <tingxinzhe1@gmail.com>
2019-09-18 23:29:31 +0800

    1

--- a.py
+++ a.py
@@ -2,7 +2,8 @@
 # 要求：打开一BMP文件，将其转存为RGB三通道图像
 #       将图像量化为0.5/0.25灰度范围的图像
 import cv2
-
+import numpy as np
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
     # 读取图片
     img = cv2.imread('wallhaven.bmp')
@@ -10,7 +11,25 @@ if __name__ == "__main__":
 
     # cv2.imread()函数读取图片后直接是BGR格式
     # 保存图片
-    cv2.imwrite('wallhaven.jpg', img)
+    # cv2.imwrite('wallhaven.jpg', img)
 
-    # 转化为灰度图
-    imgary = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # opencv转化灰度图
+    imgray0 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 手动计算转换灰度图
+    imgray1 = np.dot(img[..., :3], [0.114, 0.587, 0.299]).astype(np.uint8)
+    # print(imgray1.shape)
+
+    fig = plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.title('opencv method')
+    plt.imshow(imgray0, cmap='gray')
+    plt.axis('off')
+    plt.subplot(2, 2, 2)
+    plt.title('caculate')
+    plt.imshow(imgray1, cmap='gray')
+    plt.axis('off')
+    plt.subplot(2, 2, 3)
+    plt.hist(np.ravel(imgray0), bins=256, normed=True, color='c')
+    plt.subplot(2, 2, 4)
+    plt.hist(np.ravel(imgray1), bins=256, normed=True, color='c')
+    plt.show()
--- b.py
+++ b.py
@@ -4,17 +4,25 @@
 import numpy as np
 import cv2
 
-img = np.zeros((400, 700,3), np.uint8)
+img = np.zeros((400, 700, 3), np.uint8)
 
 img.fill(245)
 
 # 生成渐变灰度图
 for i in range(256):
-    img[i + 50,120:180,:] = 255 - i
-    img[i+50,320:]
+    img[i + 50, 120:180, :] = 255 - i
+    # 彩色渐变
+    img[i + 50, 420:480, 0] = i
+    img[i + 50, 420:480, 1] = 0
+    img[i + 50, 420:480, 2] = 255 - i
+
 # 生成等阶灰度图
 for i in range(0, 256, 32):
-    img[i+50:i+82, 220:280,:] = 255-i
+    img[i + 50:i + 82, 220:280, :] = 255 - i
+    # 彩色等阶图
+    img[i + 50:i + 82, 520:580, 0] = i
+    img[i + 50:i + 82, 520:580, 1] = 0
+    img[i + 50:i + 82, 520:580, 2] = 255 - i
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
