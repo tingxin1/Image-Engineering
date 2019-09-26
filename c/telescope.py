a97bfc3bb7d868c9c42e7aa48c3aceaa69ca4d8a
tingxinzhe <tingxinzhe1@gmail.com>
2019-09-26 23:15:08 +0800

    done

--- c/telescope.py
+++ c/telescope.py
@@ -5,21 +5,49 @@
 #           不同的色彩通道显示（红外夜视模式）
 import os
 import cv2
-import numpy as np 
+import numpy as np
 
-telescope = np.zeros((900,1440,3),dtype='uint8')
-telescope.fill(255)
-cv2.circle(telescope,(425,450),400,(0,0,0),-1)
-cv2.circle(telescope,(1025,450),400,(0,0,0),-1)
-# cv2.imshow("Circle",telescope)
-# cv2.waitKey(0)
 
-model_path=os.path.dirname(__file__)
-c2=cv2.imread(model_path+'/c2.jpg')
-c2[:,:,0].fill(0)
-c2[:,:,2].fill(0)
-# print(c2.shape)
-img=cv2.add(telescope,c2)
-cv2.namedWindow("c2",cv2.WINDOW_NORMAL)
-cv2.imshow("c2",img)
-cv2.waitKey(0)
+def gen_polygon(high, width):
+    # 生成望远镜形状的图片
+    telescope = np.zeros((high, width, 3), dtype='uint8')
+    telescope.fill(255)
+    hi = high // 2
+    wi = width // 2
+    radius = min(hi, 3*width//10)
+    cv2.circle(telescope, (wi-2*radius//3, hi), radius, (0, 0, 0), -1)
+    cv2.circle(telescope, (wi+2*radius//3, hi), radius, (0, 0, 0), -1)
+    # cv2.imshow("Circle",telescope)
+    # cv2.waitKey(0)
+    return telescope
+
+
+def green_channel(image):
+    # 只显示RGB图片中的G通道
+    image[:, :, 0].fill(0)
+    image[:, :, 2].fill(0)
+    return image
+
+
+if __name__ == "__main__":
+    model_path = os.path.dirname(__file__)
+    # 输入视频
+    cap = cv2.VideoCapture(model_path + '/example.mp4')
+    # 输出视频
+    video_out = model_path + '/telescope.mp4'
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    out = cv2.VideoWriter(video_out, fourcc, 25.0, (852, 480))
+    # 望远镜图片
+    telescope = gen_polygon(480, 852)
+    while (cap.isOpened()):
+        rat, frame = cap.read()
+        # 判断视频是否结束
+        if not rat:
+            break
+        frame_new = cv2.add(telescope, green_channel(frame))
+        cv2.imshow("processed", frame_new)
+        out.write(frame_new)
+        cv2.waitKey(50)
+    cap.release()
+    # out.release()
+    cv2.destroyAllWindows()
