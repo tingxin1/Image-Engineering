0d33bace29b2b96a5a7486ff18aab17c18757960
tingxinzhe <tingxinzhe1@gmail.com>
2019-09-29 20:23:10 +0800

    done

--- d/histogram_equalization.py
+++ d/histogram_equalization.py
@@ -6,67 +6,85 @@
 
 import os
 import cv2
-import copy
+import copy as cp
 import numpy as np
 from matplotlib import pyplot as plt
 
-def hist_eq(image):
+
+def hist_eq(image, gray_min=0, gray_max=255):
     # 直方图均衡化
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgArray = imgGray.flatten()
+    # image: opencv读入的BGR三通道图片
+    # gray_min: 直方图均衡化后灰度值下界，默认为0
+    # gray_max: 直方图均衡化后灰度值上界，默认为255
 
+    # 转为灰度图
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    height, width = img_gray.shape
+    pixel_sum = height*width
     # 统计不同灰度级的像素数
-    grayNum = np.zeros(256, dtype=int)
-    for pixel in imgArray:
-        grayNum[pixel] += 1
-
+    gray_num = np.zeros(256, dtype=int)
+    for i in range(height):
+        for j in range(width):
+            gray_num[img_gray[i, j]] += 1
     # 计算每个灰度级的概率
     probability = np.zeros(256, dtype=float)
     for i in range(0, 255):
-        probability[i] = grayNum[i]/imgArray.size
-
+        probability[i] = gray_num[i] / pixel_sum
     # 统计每个灰度级的累计概率
-    probabilitySum = np.zeros(256, dtype=float)
+    probability_sum = np.zeros(256, dtype=float)
     for i in range(0, 256):
         for j in range(0, i):
-            probabilitySum[i] += probability[j]
+            probability_sum[i] += probability[j]
+    # 直方图均衡化
+    img_hist_eq = cp.deepcopy(img_gray)
+    for i in range(height):
+        for j in range(width):
+            img_hist_eq[i, j] = probability_sum[img_gray[i, j]]*(gray_max-gray_min)+gray_min
+    return img_gray, img_hist_eq
 
 
+if __name__ == "__main__":
+    module_path = os.path.dirname(__file__)
+    filein = module_path+'/example.png'
+    img = cv2.imread(filein)
+    img_O, img_HE = hist_eq(img)
+    img_O_hist = img_O.flatten()
+    img_HE_hist = img_HE.flatten()
 
+    # opencv直方图均衡化
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    eq = cv2.equalizeHist(image)
+    hist = cv2.calcHist([eq], [0], None, [256], [0, 256])
 
-
-
-
-
-module_path = os.path.dirname(__file__)
-filein = module_path+'/testimg.jpg'
-img = cv2.imread(filein)
-# cv2.imshow('y', img)
-
-
-
-
-# 直方图变换
-i = 0
-imgArrayEnhanced = copy.deepcopy(imgArray)
-for pixel in imgArray:
-    imgArrayEnhanced[i] = probabilitySum[pixel]*255+0.5
-    i += 1
-
-# 绘制直方图
-plt.subplot(211)
-plt.xlabel('grade')
-plt.ylabel('numbers')
-plt.hist(imgArray, bins=256, density=0, edgecolor='None', facecolor='red')
-plt.subplot(212)
-plt.xlabel('grade')
-plt.ylabel('numbers')
-plt.hist(imgArrayEnhanced, bins=256, density=0,
-         edgecolor='None', facecolor='red')
-plt.show()
-
-imgGrayEnhanced = imgArrayEnhanced.reshape(512, 512)
-
-cv2.imshow('enhanced', imgGrayEnhanced)
-cv2.imshow('before', imgGray)
-cv2.waitKey(0)
+    # 绘制直方图
+    plt.figure()
+    plt.subplot(321)
+    plt.title('original')
+    plt.imshow(img_O, cmap='gray')
+    plt.axis('off')
+    plt.subplot(322)
+    plt.xlabel('Bins')
+    plt.ylabel('# of Pixels')
+    plt.hist(img_O_hist, bins=256, density=0,
+             edgecolor='None', facecolor='black')
+    plt.xlim([0, 256])
+    plt.subplot(323)
+    plt.title('histogram_equalization')
+    plt.imshow(img_HE, cmap='gray')
+    plt.axis('off')
+    plt.subplot(324)
+    plt.xlabel('Bins')
+    plt.ylabel('# of Pixels')
+    plt.hist(img_HE_hist, bins=256, density=0,
+             edgecolor='None', facecolor='red')
+    plt.xlim([0, 256])
+    plt.subplot(325)
+    plt.title('openCV histogram equalization')
+    plt.imshow(eq, cmap='gray')
+    plt.axis('off')
+    plt.subplot(326)
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
+    plt.plot(hist)
+    plt.xlim([0, 256])
+    plt.show()
