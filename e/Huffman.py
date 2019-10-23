3d319bdeb0e96feafe18b90c020e84e22ec64b62
tingxinzhe <tingxinzhe1@gmail.com>
2019-10-24 00:14:34 +0800

    just

--- e/Huffman.py
+++ e/Huffman.py
@@ -1,114 +1,145 @@
-'''读取一幅图像，进行哈弗曼编码/解码'''
+# -*- coding: utf-8 -*-
 import os
 import cv2
-import numpy as np
+import numpy
+import pickle
+import sys
+from PIL import Image
 
-
-class node:
-    # 构造用以表示节点的类
-    def __init__(self, right=None, left=None, parent=None, weight=0,
-                 code=None):  # 节点构造方法
+class node():
+    def __init__(self, right=None, left=None, parent=None, weight=0, code=None, length=0):
         self.left = left
         self.right = right
         self.parent = parent
         self.weight = weight
         self.code = code
+        self.length = length
 
 
-def pixel_statistics(picdata):
-    # 统计每个像素出现的次数,并按从小到大排序
-    # 将数组转化为列表
-    picdata = np.ravel(picdata).tolist()
-    dict = {}
-    for key in picdata:
-        dict[key] = dict.get(key, 0)+1
-    pix_sta_sorted = sorted(dict.items(), key=lambda item: item[1])
-    return pix_sta_sorted
+def pixelStat(listP):
+    static = {}
+    for i in listP:
+        if i not in static.keys():
+            static[i] = 1
+        else:
+            static[i] += 1
+    return static
 
 
-def construct_leaf_nodes(xiang_su_zhi):
-    """
-    构造叶子节点，分别赋予其像素点的值和像素点的权重
-    code = 像素点的值
-    weight = 像素点的权重
-    :param xiang_su_zhi:
-    :return:
-    """
-    nodes_list = [node(weight=xiang_su_zhi[i][1], code=str(xiang_su_zhi[i][0]))
-                  for i in range(len(xiang_su_zhi))]
-    # print node_list.__len__()    # 256
-    # print node_list[0].code, node_list[0].weight   # 255 26
-    return nodes_list
-
-
-def huffman_tree(listnode):
-    """
-    根据叶子结点列表，生成对应的霍夫曼编码树
-    :param listnode:
-    :return:
-    """
-    # listnode = sort_by_weight(listnode)
-
-    while len(listnode) != 1:
-        low_node0, low_node1 = listnode[0], listnode[1]  # 每次取最小权值的两个像素点进行合并
-        new_change_node = node()
-        new_change_node.weight = low_node0.weight + low_node1.weight
-        new_change_node.left = low_node0
-        new_change_node.right = low_node1
-        low_node0.parent = new_change_node
-        low_node1.parent = new_change_node
-        # remove() 函数用于移除列表中某个值的第一个匹配项
-        listnode.remove(low_node0)
-        listnode.remove(low_node1)
-        listnode.append(new_change_node)
-        listnode = sorted(listnode, key=lambda node: node.weight)
-    return listnode  # 返回头结点
-
-
-def huffman_encode(picture):
-    # 哈夫曼编码
-
-    height, width = picture.shape
-    # 统计每个像素出现的次数并排序
-    pix_sta_sorted = pixel_statistics(picture)
-
-    # 构造叶子节点
-    leaf_nodes_list = construct_leaf_nodes(pix_sta_sorted)
-
-    # 根据叶子结点列表，生成对应的霍夫曼编码树
-    head = huffman_tree(leaf_nodes_list)[0]  # 保存编码树的头结点return <node instance>
-
-    # TODO:huffman 编码规则的正确性，不同角度去理解
-    global bian_ma_biao
-    # 遍历所有的叶子节点进行编码
-    for e in leaf_nodes_list:  # 构造编码表
-        new_change_node = e
-        bian_ma_biao.setdefault(e.code, "")
-        while new_change_node != head:
-            if new_change_node.parent.left == new_change_node:
-                bian_ma_biao[e.code] = "1" + bian_ma_biao[e.code]
+def buildTree(pic):
+    width, height = pic.shape
+    listP = [pic[i][j] for i in range(width) for j in range(height)]
+    freq = pixelStat(listP)
+    freq = sorted(freq.items(), key=lambda x: x[1])
+    # print(freq)
+    global nodeList
+    nodeList = [node(weight=freq[i][1], code=freq[i][0])
+                for i in range(len(freq))]
+    nodeListC = nodeList
+    while(len(nodeList) != 1):
+        newNode = node()
+        node1, node2 = nodeList[0], nodeList[1]
+        newNode.weight = node1.weight + node2.weight
+        newNode.left = node1
+        newNode.right = node2
+        node1.parent = newNode
+        node2.parent = newNode
+        nodeList.remove(node1)
+        nodeList.remove(node2)
+        nodeList.append(newNode)
+        nodeList = sorted(nodeList, key=lambda x: x.weight)
+    # print(len(nodeList))
+    root = nodeList[0]
+    # print(freq)
+    codeTable = {}
+    for e in nodeListC:
+        nowNode = e
+        codeTable.setdefault(e.code, '')
+        while(nowNode != root):
+            if nowNode.parent.left == nowNode:
+                codeTable[e.code] = "1" + codeTable[e.code]
+                e.length += 1
             else:
-                bian_ma_biao[e.code] = "0" + bian_ma_biao[e.code]
-            new_change_node = new_change_node.parent
-    for key in bian_ma_biao.keys():
-        print("信源像素点 {0} Huffman编码后的码字为： {1}".format(key, bian_ma_biao[key]))
-    result = ''  # 编码结果，对每个像素点进行霍夫曼编码
+                codeTable[e.code] = "0" + codeTable[e.code]
+                e.length += 1
+            nowNode = nowNode.parent
+    # print(codeTable)
+    k = 0
+    result = ''
     for i in range(width):
         for j in range(height):
-            for key, values in bian_ma_biao.iteritems():
-                if str(im[i, j]) == key:
-                    result = result + values
+            for key, value in codeTable.items():
+                # print(type(key))
+                # print(pic[i][j],key)
+                if pic[i][j] == key:
+                    # print(pic[i][j],key)
+                    # print(1)
+                    result = result + value
+                    k += 1
+
     with open('result.txt', 'w') as f:
         f.write(result)
-    print("您的编码表为:", bian_ma_biao)
+    f.close()
+    with open('codeTable.pkl', 'wb') as f:
+        pickle.dump(codeTable, f)
+    with open('eNumber.txt', 'w') as f1:
+        f1.write(str(k))
+    f1.close
+
+    # 计算平均编码长度
+    avrLength = 0
+    all = width*height
+    for e in nodeListC:
+        # print(e.length,e.weight,all)
+        avrLength += e.length * e.weight / all
+    print("平均编码长度为：", avrLength)
+    return k, codeTable
+    # print(codeTable)
+    # print(root.code,codeTable[root.code],root.weight)
 
 
-if __name__ == "__main__":
-    model_path = os.path.dirname(__file__)
-    img = cv2.imread(model_path + '/example.png', flags=0)
-    # print(img.shape)
-    # cv2.imshow("123",img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # print(pixel_statistics(img))
-    huffman_encode(img)
+def recover(width, height, eN):
+    with open('result.txt', 'r') as p:
+        zifuchuan = p.readlines()[0].strip('\n')
+    # print(width,height)
+    with open('codeTable.pkl', 'rb+') as codeTable1:
+        codeTable = pickle.load(codeTable1, encoding='UTF-8')
+    # print(codeTable)
+    i = 0
+    Sao_miao = ''
+    Huan_yuan_xiang_su = []
+    while i != zifuchuan.__len__():  # Decode using the encoding table
+        Sao_miao = Sao_miao + zifuchuan[i]
+        for key in codeTable.keys():
+            if Sao_miao == codeTable[key]:
+                Huan_yuan_xiang_su.append(key)
+                Sao_miao = ''
+                break
+        i += 1
+    # print(len(Huan_yuan_xiang_su))
+    y = width
+    x = height
+    c = Image.new('L', (x, y))
+    k = 0
+    for i in range(x):
+        for j in range(y):
+            c.putpixel((j, i), (int(Huan_yuan_xiang_su[k])))
+            k += 1
+            if k == eN:
+                break
+    c.save('huffman_restore_photo' + '.bmp')
+    print("Your decoding is complete:" +
+          "The image is stored as huffman_restore_photo.bmp")
+
+
+if __name__ == '__main__':
+    pic = cv2.imread(os.path.dirname(__file__) + "/example.png", flags=0)
+    width, height = pic.shape
+    eNumber, codeTable = buildTree(pic)
+    print("Your encoding is complete")
+    width =130
+    height =130 
+    with open('eNumber.txt', 'r') as p:
+        zifuchuan = p.readlines()[0].strip('\n')
+    eNumber = int(zifuchuan)
+    recover(width, height, eNumber)
